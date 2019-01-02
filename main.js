2de38706db62f2a4c6396750d28f81af924296f5
Kevin Carter <kevin.carter@rackspace.com>
2019-01-02 15:38:17 -0600

    Allow multi-line pastes
    The plugin will now handle pastes that are multi-line on all sites that
    have permission to do so. In the event of an error the plugin will log
    the error and fall back to the original selectionText method.
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function makeXhrPostRequest(pastedContent) {
+makeXhrPostRequest = function (pastedContent) {
   return new Promise(
     resolve => {
       let xhr = new XMLHttpRequest();
       xhr.open('POST', 'https://pasted.tech/api/pastes', true);
       xhr.setRequestHeader('Content-Type', 'application/json')
-      xhr.onload = function() {
+      xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-            return resolve(xhr.response);
+          return resolve(xhr.response);
         }
       }
       xhr.send(
@@ -18,10 +18,9 @@ function makeXhrPostRequest(pastedContent) {
       )
     }
   )
-}
+};
 
-postPasted = function(word) {
-  var pastedContent = word.selectionText;
+runPastedPost = function (pastedContent) {
   makeXhrPostRequest(pastedContent).then(data => {
     // Add the raw URL to the paste buffer
     var copyTextArea = document.createElement('textarea');
@@ -44,12 +43,37 @@ postPasted = function(word) {
 
     // Open new paste in a browser tab
     var pastedUrl = data.replace(/\.[^/.]+$/, "");
-    chrome.tabs.create({url: pastedUrl, active: false, index: 0});
+    chrome.tabs.create({ url: pastedUrl, active: false, index: 0 });
   })
+}
+
+postPasted = function (word) {
+  chrome.tabs.getSelected(null, function (tab) {
+    chrome.tabs.executeScript(tab.id, { code: "window.getSelection().toString();" }, function (selection) {
+      if (chrome.runtime.lastError) {
+        console.log(
+          'Error executing "window.getSelection", Error Message: ' + '"' + chrome.runtime.lastError.message + '"'
+        );
+        runPastedPost(word.selectionText);
+      } else {
+        runPastedPost(selection[0]);
+      };
+    });
+  });
 };
 
-chrome.contextMenus.create({
-  title: "Post content to pasted.tech",
-  contexts:["selection"],
-  onclick: postPasted
+// Set up context menu at install time.
+chrome.runtime.onInstalled.addListener(function () {
+  var context = ["selection"];
+  var title = "Post content to pasted.tech";
+  var id = chrome.contextMenus.create(
+    {
+      "title": title,
+      "contexts": context,
+      "id": "context" + context[0]
+    }
+  );
+  return id;
 });
+
+chrome.contextMenus.onClicked.addListener(postPasted);
