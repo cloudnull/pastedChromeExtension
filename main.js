9ecc823e85f070f30563a32539d4e424e6edb44f
Kevin Carter <kevin.carter@rackspace.com>
2019-01-02 10:59:31 -0600

    Add notificiations and better tab management
    The pasted plugin will now notify the user that content has been added
    to the paste buffer and open a tab at index 0 that is NOT active. This
    ensures the users is not navigated away from the content they're working
    on while also making it possible to rapidly share links to raw content
    with others.
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- main.js
+++ main.js
@@ -23,8 +23,28 @@ function makeXhrPostRequest(pastedContent) {
 postPasted = function(word) {
   var pastedContent = word.selectionText;
   makeXhrPostRequest(pastedContent).then(data => {
-    var pastedUrl = data.replace(/\.[^/.]+$/, "")
-    chrome.tabs.create({url: pastedUrl});
+    // Add the raw URL to the paste buffer
+    var copyTextArea = document.createElement('textarea');
+    copyTextArea.style.position = 'fixed';
+    copyTextArea.style.opacity = 0;
+    copyTextArea.value = data;
+    document.body.appendChild(copyTextArea);
+    copyTextArea.select();
+    document.execCommand('Copy');
+    document.body.removeChild(copyTextArea);
+
+    // Notify user content has been copied
+    var notificationVar = {
+      type: 'basic',
+      iconUrl: 'assets/pencil.png',
+      title: 'Pasted URL Copied',
+      message: data
+    };
+    chrome.notifications.create('pastedNotification', notificationVar);
+
+    // Open new paste in a browser tab
+    var pastedUrl = data.replace(/\.[^/.]+$/, "");
+    chrome.tabs.create({url: pastedUrl, active: false, index: 0});
   })
 };
 
